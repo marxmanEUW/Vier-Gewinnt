e9f6673ba762375cd35e346bb6f035f52e59fd10
Patrick Kilian <patri@DESKTOP-B2DE12M>
2018-07-20 11:03:46 +0200

    Filled TcpServerClient with Code

--- VierGewinntServer/VierGewinntServer/TcpServerClient.cs
+++ VierGewinntServer/VierGewinntServer/TcpServerClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,24 @@ namespace VierGewinntServer
 {
     class TcpServerClient
     {
+        private const int PLAYER_NAME_MAX_LENGTH = 20;
+
+        private string _PlayerName = String.Empty;
+        public string PlayerName
+        {
+            get { return _PlayerName; }
+            set
+            {
+                if(value.Length > PLAYER_NAME_MAX_LENGTH)
+                {
+                    _PlayerName = value.Substring(0, PLAYER_NAME_MAX_LENGTH);
+                }
+                else
+                {
+                    _PlayerName = value;
+                }
+            }
+        }
+        public TcpClient PlayerClient { get; set; }        
     }
 }

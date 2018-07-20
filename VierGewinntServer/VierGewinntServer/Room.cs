549e6fc866730d5c30944ab785c0ac99399dd44b
marxmanEUW <conrad.ostertag@gmail.com>
2018-07-20 10:57:58 +0200

    Added more functions to Room Class

--- VierGewinntServer/VierGewinntServer/Room.cs
+++ VierGewinntServer/VierGewinntServer/Room.cs
@@ -13,11 +13,12 @@ namespace VierGewinntServer
         public const int NUMBER_OF_COLUMNS = 7;
 
         public enum RoomState { WAITING_FOR_SECOND_PLAYER, PLAYING, FINISHED }
+        public enum TurnState { VALID, NOT_VALID, NOT_ACITVE_PLAYER, UNDEFINIED_PLAYER}
         
         public string Id { get; private set; }
         public string Name { get; set; }
         public RoomState Status { get; private set; }
-        public int[,] PlayGround { get; private set; }
+        public int[,] PlayGround { get; private set; } //[Row, Column]
 
         public TcpClient ActivePlayer { get; private set; }
         public TcpClient Player1 { get; private set; }
@@ -26,31 +27,30 @@ namespace VierGewinntServer
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="Name"></param>
-        /// <param name="Player1"></param>
-        public Room(string Name, TcpClient Player1)
+        /// <param name="name"></param>
+        /// <param name="player1"></param>
+        public Room(string name, TcpClient player1)
         {
             string Ticks = DateTime.Now.Ticks.ToString();
             Random Rand = new Random();
             string RandomString = Rand.Next(1000, 9999).ToString();
             this.Id = Ticks + RandomString;
 
-            this.Player1 = Player1;
-            this.Name = Name;
+            this.Player1 = player1;
+            this.Name = name;
             this.Status = RoomState.WAITING_FOR_SECOND_PLAYER;
-            
         }
 
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="Player2"></param>
+        /// <param name="player2"></param>
         /// <returns></returns>
-        public bool AddSecondPlayer(TcpClient Player2)
+        public bool AddSecondPlayer(TcpClient player2)
         {
             if (this.Status.Equals(RoomState.WAITING_FOR_SECOND_PLAYER) && this.Player2 == null)
             {
-                this.Player2 = Player2;
+                this.Player2 = player2;
                 this.Status = RoomState.PLAYING;
                 this.ActivePlayer = this.Player1;
                 this.PlayGround = new int[NUMBER_OF_ROWS, NUMBER_OF_COLUMNS];
@@ -63,36 +63,31 @@ namespace VierGewinntServer
         }
 
         
-        public bool NextTurn(TcpClient Player, int Row, int Column)
+        public TurnState NextTurn(TcpClient player, int column)
         {
-            if (Player == this.ActivePlayer)
+            if (player == this.ActivePlayer)
             {
-                int PlayerNumber;
+                int playerNumber;
                 if (this.ActivePlayer == this.Player1)
                 {
-                    PlayerNumber = 1;
+                    playerNumber = 1;
                 }
                 else if (this.ActivePlayer == this.Player2)
                 {
-                    PlayerNumber = 2;
+                    playerNumber = 2;
                 }
                 else
                 {
-                    PlayerNumber = 0;
+                    playerNumber = 0;
                     Console.WriteLine("ERROR: Active Player is neither Player1 nor Player2!");
-                    return false;
+                    return TurnState.UNDEFINIED_PLAYER;
                 }
 
-                if (this.PlayGround[Row,Column] == 0)
-                {
-                    this.PlayGround[Row, Column] = PlayerNumber;
-                    
-                }
-                return true;
+                return this.DropPiece(playerNumber, column);
             }
             else
             {
-                return false;
+                return TurnState.NOT_ACITVE_PLAYER;
             }
         }
 
@@ -109,14 +104,35 @@ namespace VierGewinntServer
             }
             else if (this.ActivePlayer == this.Player2)
             {
-                PlayerNumber = 2;
+                this.ActivePlayer = this.Player1;
             }
             else
             {
-                PlayerNumber = 0;
+                this.ActivePlayer = this.Player1;
                 Console.WriteLine("ERROR: Active Player is neither Player1 nor Player2!");
-                return false;
             }
+        }
+
+        private TurnState DropPiece(int playerNumber, int column)
+        {
+            if (this.PlayGround[0,column] != 0)
+            {
+                return TurnState.NOT_VALID;
+            }
+
+            int newRow = 0;
+            for (int i = 1; i <= NUMBER_OF_ROWS; i++)
+            {
+                if (this.PlayGround[i, column] != 0)
+                {
+                    this.PlayGround[newRow, column] = playerNumber;
+                    return TurnState.VALID;
+                }
+                newRow = i;
+            }
+
+            Console.WriteLine("ERROR: Could not drop piece!");
+            return TurnState.NOT_VALID;
         }
     }
 }

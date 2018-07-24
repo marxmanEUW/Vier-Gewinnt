eab34d8bd400f5f80351620cb2b871be85642c4b
Patrick Kilian <patri@DESKTOP-B2DE12M>
2018-07-24 11:04:48 +0200

    Added Simple Room Data Deserializer Method

---
+++ VierGewinntServer/VierGewinntServer/DataFormats/DataRoom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VierGewinntServer.DataFormats
+{
+    class DataRoom
+    {
+        public string Name { get; set; }
+    }
+}
--- VierGewinntServer/VierGewinntServer/DataProcessor.cs
+++ VierGewinntServer/VierGewinntServer/DataProcessor.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using VierGewinntServer.DataFormats;
 
 namespace VierGewinntServer
 {
     class DataProcessor
     {
-
+        string json_string = "";
+        
+        public static DataRoom DeserializeRoomData(string aJSON_STRING)
+        {
+            return JsonConvert.DeserializeObject<DataRoom>(aJSON_STRING);
+        }
     }
 }

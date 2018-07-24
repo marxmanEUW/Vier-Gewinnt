9b0d3a04ab521d8b766e2be75f5645079bbe045b
marxmanEUW <conrad.ostertag@gmail.com>
2018-07-24 15:39:56 +0200

    Added functions for de- and encrypting strings

---
+++ SecureClient/SecureClient/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace SecureClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.KlartextLabel1 = new System.Windows.Forms.Label();
+            this.KlartextTextbox1 = new System.Windows.Forms.TextBox();
+            this.ChiffreTextbox = new System.Windows.Forms.TextBox();
+            this.ChiffreLabel = new System.Windows.Forms.Label();
+            this.KlartextTextbox2 = new System.Windows.Forms.TextBox();
+            this.KlartextLabel2 = new System.Windows.Forms.Label();
+            this.VerschluesselnButton = new System.Windows.Forms.Button();
+            this.EntschluesselnButton = new System.Windows.Forms.Button();
+            this.SchluesselTextbox = new System.Windows.Forms.TextBox();
+            this.SchluesselLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // KlartextLabel1
+            // 
+            this.KlartextLabel1.AutoSize = true;
+            this.KlartextLabel1.Location = new System.Drawing.Point(60, 177);
+            this.KlartextLabel1.Name = "KlartextLabel1";
+            this.KlartextLabel1.Size = new System.Drawing.Size(66, 20);
+            this.KlartextLabel1.TabIndex = 0;
+            this.KlartextLabel1.Text = "Klartext:";
+            // 
+            // KlartextTextbox1
+            // 
+            this.KlartextTextbox1.Location = new System.Drawing.Point(171, 174);
+            this.KlartextTextbox1.Name = "KlartextTextbox1";
+            this.KlartextTextbox1.Size = new System.Drawing.Size(373, 26);
+            this.KlartextTextbox1.TabIndex = 1;
+            // 
+            // ChiffreTextbox
+            // 
+            this.ChiffreTextbox.Location = new System.Drawing.Point(171, 225);
+            this.ChiffreTextbox.Name = "ChiffreTextbox";
+            this.ChiffreTextbox.Size = new System.Drawing.Size(373, 26);
+            this.ChiffreTextbox.TabIndex = 3;
+            // 
+            // ChiffreLabel
+            // 
+            this.ChiffreLabel.AutoSize = true;
+            this.ChiffreLabel.Location = new System.Drawing.Point(60, 228);
+            this.ChiffreLabel.Name = "ChiffreLabel";
+            this.ChiffreLabel.Size = new System.Drawing.Size(60, 20);
+            this.ChiffreLabel.TabIndex = 2;
+            this.ChiffreLabel.Text = "Chiffre:";
+            // 
+            // KlartextTextbox2
+            // 
+            this.KlartextTextbox2.Location = new System.Drawing.Point(171, 282);
+            this.KlartextTextbox2.Name = "KlartextTextbox2";
+            this.KlartextTextbox2.Size = new System.Drawing.Size(373, 26);
+            this.KlartextTextbox2.TabIndex = 5;
+            // 
+            // KlartextLabel2
+            // 
+            this.KlartextLabel2.AutoSize = true;
+            this.KlartextLabel2.Location = new System.Drawing.Point(60, 285);
+            this.KlartextLabel2.Name = "KlartextLabel2";
+            this.KlartextLabel2.Size = new System.Drawing.Size(66, 20);
+            this.KlartextLabel2.TabIndex = 4;
+            this.KlartextLabel2.Text = "Klartext:";
+            // 
+            // VerschluesselnButton
+            // 
+            this.VerschluesselnButton.Location = new System.Drawing.Point(609, 170);
+            this.VerschluesselnButton.Name = "VerschluesselnButton";
+            this.VerschluesselnButton.Size = new System.Drawing.Size(122, 35);
+            this.VerschluesselnButton.TabIndex = 6;
+            this.VerschluesselnButton.Text = "Verschlüsseln";
+            this.VerschluesselnButton.UseVisualStyleBackColor = true;
+            this.VerschluesselnButton.Click += new System.EventHandler(this.VerschluesselnButton_Click);
+            // 
+            // EntschluesselnButton
+            // 
+            this.EntschluesselnButton.Location = new System.Drawing.Point(609, 221);
+            this.EntschluesselnButton.Name = "EntschluesselnButton";
+            this.EntschluesselnButton.Size = new System.Drawing.Size(122, 35);
+            this.EntschluesselnButton.TabIndex = 7;
+            this.EntschluesselnButton.Text = "Entschlüsseln";
+            this.EntschluesselnButton.UseVisualStyleBackColor = true;
+            this.EntschluesselnButton.Click += new System.EventHandler(this.EntschluesselnButton_Click);
+            // 
+            // SchluesselTextbox
+            // 
+            this.SchluesselTextbox.Location = new System.Drawing.Point(171, 84);
+            this.SchluesselTextbox.Name = "SchluesselTextbox";
+            this.SchluesselTextbox.Size = new System.Drawing.Size(373, 26);
+            this.SchluesselTextbox.TabIndex = 9;
+            // 
+            // SchluesselLabel
+            // 
+            this.SchluesselLabel.AutoSize = true;
+            this.SchluesselLabel.Location = new System.Drawing.Point(60, 87);
+            this.SchluesselLabel.Name = "SchluesselLabel";
+            this.SchluesselLabel.Size = new System.Drawing.Size(81, 20);
+            this.SchluesselLabel.TabIndex = 8;
+            this.SchluesselLabel.Text = "Schlüssel:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 615);
+            this.Controls.Add(this.SchluesselTextbox);
+            this.Controls.Add(this.SchluesselLabel);
+            this.Controls.Add(this.EntschluesselnButton);
+            this.Controls.Add(this.VerschluesselnButton);
+            this.Controls.Add(this.KlartextTextbox2);
+            this.Controls.Add(this.KlartextLabel2);
+            this.Controls.Add(this.ChiffreTextbox);
+            this.Controls.Add(this.ChiffreLabel);
+            this.Controls.Add(this.KlartextTextbox1);
+            this.Controls.Add(this.KlartextLabel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label KlartextLabel1;
+        private System.Windows.Forms.TextBox KlartextTextbox1;
+        private System.Windows.Forms.TextBox ChiffreTextbox;
+        private System.Windows.Forms.Label ChiffreLabel;
+        private System.Windows.Forms.TextBox KlartextTextbox2;
+        private System.Windows.Forms.Label KlartextLabel2;
+        private System.Windows.Forms.Button VerschluesselnButton;
+        private System.Windows.Forms.Button EntschluesselnButton;
+        private System.Windows.Forms.TextBox SchluesselTextbox;
+        private System.Windows.Forms.Label SchluesselLabel;
+    }
+}
+
---
+++ SecureClient/SecureClient/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecureClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private byte[] DoExtendKey(string key, int newKeyLength)
+        {
+            byte[] bKey = new byte[newKeyLength];
+            byte[] tmpKey = Encoding.UTF8.GetBytes(key);
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                bKey[i] = tmpKey[i];
+            }
+
+            return bKey;
+        }
+
+        private byte[] DoCreateBlocksize(int newBlocksize)
+        {
+            byte[] block = new byte[newBlocksize];
+
+            for (byte i = 0; i < newBlocksize; i++)
+            {
+                block[i] = i;
+            }
+
+            return block;
+        }
+
+        private string AesDecrypt(string plainText, string key)
+        {
+            Aes AESCrypto = Aes.Create();
+            AESCrypto.Key = DoExtendKey(key, 32);
+            AESCrypto.IV = DoCreateBlocksize(16);
+
+            MemoryStream mStream = new MemoryStream();
+            CryptoStream cStream = new CryptoStream(mStream, AESCrypto.CreateEncryptor(), CryptoStreamMode.Write);
+
+            byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+            cStream.Write(plainBytes, 0, plainBytes.Length);
+            cStream.FlushFinalBlock();
+
+            byte[] encryptedBytes = mStream.ToArray();
+            string encryptedString = Convert.ToBase64String(encryptedBytes);
+
+            mStream.Close();
+            cStream.Close();
+
+            return encryptedString;
+        }
+
+        private string AesEncrypt(string encryotedText, string key)
+        {
+            Aes AESCrypto = Aes.Create();
+            AESCrypto.Padding = PaddingMode.Zeros;
+            AESCrypto.Key = DoExtendKey(key, 32);
+            AESCrypto.IV = DoCreateBlocksize(16);
+
+            MemoryStream mStream = new MemoryStream();
+            CryptoStream cStream = new CryptoStream(mStream, AESCrypto.CreateDecryptor(), CryptoStreamMode.Write);
+
+            string plainText = "";
+
+            try
+            {
+                byte[] encryptedBytes = Convert.FromBase64String(encryotedText);
+                cStream.Write(encryptedBytes, 0, encryptedBytes.Length);
+                cStream.FlushFinalBlock();
+
+                byte[] plainBytes = mStream.ToArray();
+                plainText = Encoding.UTF8.GetString(plainBytes);
+            }
+            catch
+            {
+                MessageBox.Show("Fehlerhafte Chiffre!", "", MessageBoxButtons.OK);
+                cStream.FlushFinalBlock();
+            }
+
+            mStream.Close();
+            cStream.Close();
+
+            return plainText;
+        }
+
+        private void VerschluesselnButton_Click(object sender, EventArgs e)
+        {
+            ChiffreTextbox.Text = AesDecrypt(KlartextTextbox1.Text, SchluesselTextbox.Text);
+        }
+
+        private void EntschluesselnButton_Click(object sender, EventArgs e)
+        {
+            KlartextTextbox2.Text = AesEncrypt(ChiffreTextbox.Text, SchluesselTextbox.Text);
+        }
+    }
+}
